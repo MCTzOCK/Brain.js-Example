5a992ef05f3dbe83f7b2fcd5dabf23ee1ef3654d
MCTzOCK <dev@mctzock.de>
2021-05-17 08:18:55 +0200

    Added script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+/**
+ * @author Ben Siebert
+ * @copyright 2018-2021 Ben Siebert. All rights reserved.
+ */
+// create the network
+let network = new brain.recurrent.LSTM(); // LSTM -> Long Short Term Memory
+
+// download the trained data
+let trainingDataDownloadRequest = new XMLHttpRequest();
+trainingDataDownloadRequest.open("GET", "learned.json", false);
+trainingDataDownloadRequest.send(null);
+
+if(trainingDataDownloadRequest.status === 200){
+    // everything worked fine
+    // load trained data
+    network.fromJSON(JSON.parse(trainingDataDownloadRequest.responseText));
+}else {
+    // the training data was not found.
+    // download dataset
+    let dataSetDownloadRequest = new XMLHttpRequest();
+    dataSetDownloadRequest.open("GET", "dataset.json", false);
+    dataSetDownloadRequest.send(null);
+    // train
+    network.train(JSON.parse(dataSetDownloadRequest.response), {
+        iterations: 20000, // the maximum times to iterate the training data
+        log: true,
+        logPeriod: 10,
+    });
+}
